ed0e5b627641458d859ce9ec05010e93cb4ae2b4
koji inui <koji.inui77@gmail.com>
2016-05-11 15:58:31 +0900

    user added5

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :conditions
   resources :users
-  get 'users/:id'  =>  "users#show"
+#  get 'users/:id'  =>  "users#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
