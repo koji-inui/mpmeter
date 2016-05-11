843928c9f90dd4c1f90556fd27989ceddc0b01d1
koji inui <koji.inui77@gmail.com>
2016-05-11 17:33:44 +0900

    mypage2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :conditions
-#  resources :users, :only => [:show]
-  resources :users
+  resources :users, :only => [:show]
+#  resources :users
   root "conditions#index"
   get 'users/:id'  =>  "users#show"
   # The priority is based upon order of creation: first created -> highest priority.
