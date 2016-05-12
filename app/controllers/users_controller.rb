fc21ef32d6fa7d10bb559a15db00fe3ad82ab6c4
koji inui <koji.inui77@gmail.com>
2016-05-12 17:40:22 +0900

    pictweet2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def show 
+    @user = User.find(current_user.id)
+    @name = current_user.email
+    @conditions = current_user.conditions.where(user_id: current_user.id) 
+  end
+end
