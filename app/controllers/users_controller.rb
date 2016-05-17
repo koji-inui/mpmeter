b7ae85404cf4e38398716d361677dc343baef040
koji inui <koji.inui77@gmail.com>
2016-05-17 18:49:54 +0900

    style1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
   def show 
+
+    require "json"
+    require "open-uri"
+    api_key = "e1a6dfa87fed1ea45838113bbdea6cf7"
+    base_url = "http://api.openweathermap.org/data/2.5/forecast"
+    response = open(base_url + "?q=Tokyo,jp&units=metric&APPID=#{api_key}")
+    tenki = JSON.parse(response.read)
+
     @user = User.find(current_user.id)
     @name = current_user.email
     #@today_mp = current_user.conditions.order(:cday DESC ).first.mp
-    @today_mp = current_user.conditions.order("cday DESC ").first.mp
-    @today_st = current_user.conditions.order("cday DESC ").first.sleep_time
-    @today_ej = current_user.conditions.order("cday DESC ").first.eja_times
-    @today_condition = choushi(@today_mp)
-    @tomorrow_mp = forecast(@today_mp,@today_st,@today_ej)
-    @tomorrow_condition = choushi(@tomorrow_mp)
+    today_mp = current_user.conditions.order("cday DESC ").first.mp
+    today_st = current_user.conditions.order("cday DESC ").first.sleep_time
+    today_ej = current_user.conditions.order("cday DESC ").first.eja_times
+    today_tk = current_user.conditions.order("cday DESC ").first.temperature
+    @today_condition = choushi(today_mp)
+
+    tomorrow_tk = tenki["list"][5]["main"]["temp"]
+
+    tomorrow_mp = forecast(today_mp,today_st,today_ej,today_tk,tomorrow_tk)
+    @tomorrow_condition = choushi(tomorrow_mp)
 
 #    binding.pry
 #    @akashi =  JSON.pretty_generate(JSON.parse(response.read)) 
@@ -30,28 +42,43 @@ class UsersController < ApplicationController
         today_condition = 'まあまあ'
       elsif today_mp == 4
         today_condition = '好調'
-      else 
+      elsif today_mp == 5
         today_condition = '絶好調'
+      else 
+        today_condition = 'よくわからない'
       end
 
       return today_condition
 
     end
 
-    def forecast(mp,st,ej)
+    def forecast(mp,st,ej,t_toda,t_tomo)
       if st > 8.0
         st_add = 2
-      elsif st  > 7.0
+      elsif st  > 6.5
         st_add = 1
       elsif st  > 5.0
         st_add = 0
-      elsif st  > 3.0
+      elsif st  > 3.5
         st_add = -1
       else
         st_add = -2
       end
 
+      tomorrow_mp += 1 if ej == 0
+
       tomorrow_mp =  mp + st_add - ej
+
+      if (t_tomo - t_toda).abs > 8.0
+        tomorrow_mp -= 1
+      end
+
+      if tomorrow_mp < 1
+        tomorrow_mp = 1
+      elsif tomorrow_mp > 5
+        tomorrow_mp = 5
+      end
+
       return tomorrow_mp
 
     end
